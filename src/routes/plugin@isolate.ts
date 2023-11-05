b48d4a71d35fba55ff2d3bebdc67ae287e9f2fe8
sakurie27 <tf1004ft@gmail.com>
2023-11-05 13:16:04 +0800

    fix: isolated origin policy

---
+++ src/routes/plugin@isolate.ts
@@ -0,0 +1,12 @@
+import type { RequestHandler } from "@builder.io/qwik-city";
+
+export const onRequest: RequestHandler = event => {
+    console.warn(event.pathname)
+    const set = {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+    }
+    for (const [key, value] of Object.entries(set)) {
+        event.headers.set(key, value)
+    }
+}
